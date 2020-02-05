832b33d8842a58fbb2f520493c6fe57aca4017cb
viktorkan <html.viktor@gmail.com>
2020-02-05 14:59:54 +0200

    make first main component

---
+++ src/components/app/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Main from '../main';
+// eslint-disable-next-line react/prop-types
+const App = ({film}) => {
+  return <Main film={film}/>;
+};
+
+export default App;
