4f6ae653b85043089ea7908ce5d9dd293ebfb040
viktor kan <viktorkan9@gmail.com>
2020-02-12 14:27:54 +0300

    add tests

--- src/components/card-film-list/card-film-list.js
+++ src/components/card-film-list/card-film-list.js
@@ -6,7 +6,7 @@ import CardFilm from "../card-film/card-film";
 const CardFilmList = ({films}) => {
   const onMouseEnter = (info) => {
     // eslint-disable-next-line no-console
-    console.log(info);
+    return info;
   };
   return <div className="catalog__movies-list">
     {films.map((filmItem, index) => {
---
+++ src/components/card-film/card-film.e2e.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import CardFilm from "./card-film";
+
+Enzyme.configure({
+  adapter: new Adapter()
+});
+
+const film = {
+  title: `Title One`
+};
+
+it(`CardFilm film e2e test`, () => {
+  const onMouseEnter = jest.fn();
+
+  const cardFilmComponent = shallow(
+      <CardFilm film={film} onMouseEnter={onMouseEnter}/>
+  );
+
+  cardFilmComponent.simulate(`mouseenter`);
+  expect(onMouseEnter).toHaveBeenCalledTimes(1);
+});
